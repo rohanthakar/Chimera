022c47809aca6dddb69b51ba0f5efaf3c49c7be5
rohanthakar <thakar.rohan@gmail.com>
2017-04-01 16:20:20 +0530

    Initial Commit

---
+++ Chimera/SigInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  Chimera
+//
+//  Created by Rohan Thakar on 29/03/17.
+//  Copyright © 2017 Reverie Works. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
