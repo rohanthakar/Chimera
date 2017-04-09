ee286616e804061a89277fb1c4b1cf9cde91ecd5
rohanthakar <thakar.rohan@gmail.com>
2017-04-09 07:09:58 +0530

    UIImagePicker Enabled

--- Chimera/SharePostVC.swift
+++ Chimera/SharePostVC.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
-class SharePostVC: UIViewController {
+class SharePostVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var addImg: UIImageView!
+    var post: Post!
+    var imagePicker: UIImagePickerController!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,14 +35,19 @@ class SharePostVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            addImg.image = image
+        }else {
+            print("ROHAN: A valid image wasnt selected")
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    @IBAction func addImageTapped(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
 }
